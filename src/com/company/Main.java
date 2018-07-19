094efbc8953a390ab108b5bfe78d2105041d376d
harshachalla <harshachalla2@gmail.com>
2018-07-19 20:51:19 +0530

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,23 @@
+package com.company;
+import java.util.Scanner;
+class Add
+{
+    String sum(String x,String y)
+    {
+        System.out.println("x+"+"+y+");
+        return(x+y);
+    }
+}
+public class Main {
+    public static void main(String[]args)
+    {
+        Scanner S= new Scanner(System.in);
+        Add ao=new Add();
+        System.out.println("enter first name");
+        String s1=S.nextLine();
+        System.out.println("enter last name");
+        String s2=S.nextLine();
+        System.out.println("hello="+ao.sum(s1,s2));
+
+    }
+}
